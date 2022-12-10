4116eb2c51999ed5ffa7fbc7c3434730e7a10a00
Shayan <s.m.shayanhussain@gmail.com>
2022-12-10 23:42:16 +0500

    added searching + partial deletion

--- BST.cpp
+++ BST.cpp
@@ -27,6 +27,58 @@ public:
         }
         return node;
     }
+    string search(string reqName){
+        if(head==NULL){
+            return NULL;
+        }
+        //add string match function
+    }
+    NodeBST<tree>* search(NodeBST<tree>* node, tree req){
+        if(node==NULL){
+            cout<<"Not found\n";
+            return NULL;
+        }
+        else if(req==node->data){
+            cout<<"Found\n";
+            // inorder(node);
+            return node;
+        }
+        else if(req< node->data){
+            return search(node->left,req);
+        }
+        else if(req > node->data){
+            return search(node->right,req);
+        }
+        else{
+            cout<<"Not Found";
+            return NULL;
+        }
+    }
+    void remove(nodeBST<tree> *node, tree data, int id){//no validation check
+        if(node==NULL){
+            cout<<"Could Not Delete\n";
+        }
+        else if(data==node->data){//delete/linked list check
+            if(!removeFromList(node,id)){//the list/node has only one employee obj
+                //normal bst node deletion
+            }
+
+        }
+        else if(data < node->data){
+            remove(node->left, data, id);
+        }
+        else if(data > node->data){
+            remove(node->right, data, id);
+        }
+    }
+    bool removeFromList(NodeBST<tree> *node, int id){
+        if(node->numOfEmployees>1){
+            node->list.remove(id);
+            node->numOfEmployees=node->numOfEmployees-1;
+            return true;
+        }
+        return false;
+    }
     
     void inorder(NodeBST<tree> *node){
         if(node==NULL){
@@ -34,7 +86,7 @@ public:
         }
         else{
             inorder(node->left);
-            cout<<"Salary: "<<node->data<<", Num of employees: "<<node->numOfEmployees<<"\n";
+            cout<<"Data: "<<node->data<<", Num of employees: "<<node->numOfEmployees<<"\n";
             node->list.Display();
             inorder(node->right);
         }
@@ -43,16 +95,22 @@ public:
 
 int main(){
     Employee a;
-    a.setSalary(30000);
+    a.setAge(30);
     Employee b;
-    b.setSalary(30000);
+    b.setAge(30);
     Employee c;
-    c.setSalary(10000);
+    c.setAge(25);
     BST<int> tree;
-    tree.head=tree.insert(tree.head,a,a.getSalary());
-    tree.insert(tree.head, c, c.getSalary());
-    tree.inorder(tree.head);
+    tree.head=tree.insert(tree.head,a,a.getAge());
+    tree.insert(tree.head, c, c.getAge());
+    // tree.inorder(tree.head);
     cout<<"--------NEW INSERTION--------\n";
-    tree.insert(tree.head, b, b.getSalary());
-    tree.inorder(tree.head);
+    tree.insert(tree.head, b, b.getAge());
+    // tree.inorder(tree.head);
+
+    NodeBST<int> *val;
+    val=tree.search(tree.head, 30);
+    if(val){
+        cout<<val->numOfEmployees;
+    }
 }
